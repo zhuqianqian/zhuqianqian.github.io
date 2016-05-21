9a27d3e1e2f9a0012bf46521ee8a6c11d9d89fc8
Qianqian Zhu <zhuqianqian.299@gmail.com>
2016-05-21 14:49:51 -0400

    Add links to social network

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function setupPage() {
 function loadSkills() {
     skillSet = ["C/C++", "Java", "Android", "Android Studio",
         "Win32 Programming", "Performance Tuning", "SQL",
-        "ABAP", "Customer Support"];
+        "ABAP", "SAP", "Customer Support"];
     ul = document.getElementById("skills");
     for (i = 0; i < skillSet.length; i++) {
         var item = document.createElement("li");
@@ -14,14 +14,42 @@ function loadSkills() {
     }
 }
 
+function loadEducation() {
+
+}
+
+function loadWorks() {
+
+}
+
+function loadProjects() {
+
+}
+
 function loadLinks() {
     links = ["https://ca.linkedin.com/in/qianqian-zhu-9b42873a",
         "https://github.com/zhuqianqian",
         "https://plus.google.com/+QianqianZhu299"
     ];
     altTexts = ["LinkedIn", "GitHub", "Google Plus"];
-    imgLinks = ["https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png",
-        "",
-        ""
+    imgLinks = ["https://static.licdn.com/scds/common/u/images/logos/linkedin/logo_in_nav_44x36.png",
+        "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+        "https://developers.google.com/+/images/branding/g+138.png"
     ]
+    widths = [44, 44, 42];
+    heights = [36, 44, 42];
+    ul = document.getElementById("links");
+    for (i = 0; i < links.length; i++) {
+        var item = document.createElement("li");
+        var href = document.createElement("a");
+        href.href = links[i];
+        var img = document.createElement("img");
+        img.width = widths[i];
+        img.height = heights[i];
+        img.src = imgLinks[i];
+        img.alt = altTexts[i];
+        href.appendChild(img);
+        item.appendChild(href);
+        ul.appendChild(item);
+    }
 }
